120761a5425a818eeb0ba32b5a33901e099b0a04
mslep <v-mslepko@expediagroup.com>
2023-01-05 18:20:15 +0200

    feat(back): feed post and global error handling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+const MONGODB_URI =
+  'mongodb+srv://node-complete:nodecomplete@cluster0.etxrz.azure.mongodb.net/messages?retryWrites=true&w=majority';
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,6 +25,22 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080, () => {
-  console.log('Server is running');
+app.use((error, req, res, next) => {
+  console.log(error);
+  const { statusCode: status = 500, message } = error;
+  res.status(status).json({ message });
 });
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    app.listen(8080, () => {
+      console.log('Server is running');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -1,23 +1,52 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: 1,
         title: 'First post',
         content: 'This is the first post',
+        imageUrl: 'images/book.jpeg',
+        createdAt: new Date(),
+        creator: {
+          name: 'Mykola',
+        },
       },
     ],
   });
 };
 
 exports.createPosts = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
-  res.status(201).json({
-    message: 'Post created successfully',
-    post: {
-      id: new Date().toString(),
-      title: title,
-      content: content,
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect');
+    error.statusCode = 422;
+    throw error;
+  }
+  const { title, content } = req.body;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/car.jpeg',
+    creator: {
+      name: 'Mykola',
     },
   });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: 'Post created successfully',
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
