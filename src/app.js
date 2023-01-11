02888a730d653dadaa7843ed6af80ce4df26c4bd
mslep <v-mslepko@expediagroup.com>
2023-01-11 15:31:40 +0200

    feat(back): user model/route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const MONGODB_URI =
@@ -51,6 +52,7 @@ app.use((req, res, next) => {
 
 app.use('/src/images', express.static(path.join(__dirname, 'images')));
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
